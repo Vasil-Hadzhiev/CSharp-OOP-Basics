94d4641501a3ac76a456ec4b7a21d47d4f84cbea
Vasil <vsl.h.94@gmail.com>
2017-07-03 23:11:23 +0200

    Completed Encapsulation lab

---
+++ Encapsulation/EncapsulationLAB/04.FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.FirstAndReserveTeam
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var team = new Team("Bojurishte");
+
+            for (int i = 0; i < n; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+
+                var player = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        double.Parse(cmdArgs[3]));
+
+                team.AddPlayer(player);
+            }
+
+            Console.WriteLine(team.ToString());
+        }
+    }
+}
---
+++ Encapsulation/EncapsulationLAB/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        this.name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+    }
+
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get { return this.firstTeam.AsReadOnly(); }
+    }
+
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get { return this.reserveTeam.AsReadOnly(); }
+    }
+
+    public void AddPlayer(Person player)
+    {
+        if (player.Age < 40)
+        {
+            this.firstTeam.Add(player);
+        }
+        else
+        {
+            this.reserveTeam.Add(player);
+        }
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"First team have {this.firstTeam.Count} players");
+        sb.AppendLine($"Reverse team have {this.reserveTeam.Count} players");
+
+        return sb.ToString();
+    }
+}
