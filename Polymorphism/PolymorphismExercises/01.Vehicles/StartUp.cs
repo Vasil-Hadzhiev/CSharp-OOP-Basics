c62e786ecf78b20b0087762e1a0e4c4a117617e8
Vasil <vsl.h.94@gmail.com>
2017-07-13 09:46:16 +0200

    Completed exercise 1

---
+++ Polymorphism/PolymorphismExercises/01.Vehicles/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var carInfo = Console.ReadLine()
+            .Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        var carFuelQuantity = double.Parse(carInfo[1]);
+        var carFuelConsumption = double.Parse(carInfo[2]);
+        var car = new Car(carFuelQuantity, carFuelConsumption);
+
+        var truckInfo = Console.ReadLine()
+            .Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        var truckFuelQuantity = double.Parse(truckInfo[1]);
+        var truckFuelConsumption = double.Parse(truckInfo[2]);
+        var truck = new Truck(truckFuelQuantity, truckFuelConsumption);
+
+        var numberOfCommands = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCommands; i++)
+        {
+            var inputLine = Console.ReadLine().Split();
+
+            var command = inputLine[0];
+            var vehicleType = inputLine[1];
+            var value = double.Parse(inputLine[2]);
+
+            if (command == "Drive")
+            {
+                switch (vehicleType)
+                {
+                    case "Car":
+                        car.Drive(value);
+                        break;
+                    case "Truck":
+                        truck.Drive(value);
+                        break;
+                }
+            }
+            else
+            {
+                switch (vehicleType)
+                {
+                    case "Car":
+                        car.Refuel(value);
+                        break;
+                    case "Truck":
+                        truck.Refuel(value);
+                        break;
+                }
+            }
+        }
+
+        Console.WriteLine(car);
+        Console.WriteLine(truck);
+    }
+}
+
---
+++ Polymorphism/PolymorphismExercises/01.Vehicles/Truck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Truck : Vehicle
+{
+    public Truck(double fuelQuantity, double fuelConsumption) 
+        : base(fuelQuantity, fuelConsumption)
+    {
+    }
+
+    public override double FuelConsumption => base.FuelConsumption + 1.6;
+
+    public override void Drive(double distance)
+    {
+        if (this.FuelConsumption * distance > this.FuelQuantity)
+        {
+            Console.WriteLine("Truck needs refueling");
+        }
+        else
+        {
+            this.FuelQuantity -= this.FuelConsumption * distance;
+            Console.WriteLine($"Truck travelled {distance} km");
+        }
+    }
+
+    public override void Refuel(double liters)
+    {
+        this.FuelQuantity += liters * 0.95;
+    }
+
+    public override string ToString()
+    {
+        return $"Truck: {this.FuelQuantity:f2}";
+    }
+}
+
