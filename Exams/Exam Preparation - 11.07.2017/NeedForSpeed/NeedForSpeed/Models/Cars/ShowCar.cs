0fe452f2bcbbfcfe0334cf89f2d9c3d7e930b7dd
Vasil <vsl.h.94@gmail.com>
2017-08-28 12:13:51 +0200

    Completed exam prep and refactored code

---
+++ Exams/Exam Preparation - 11.07.2017/NeedForSpeed/NeedForSpeed/Models/Cars/ShowCar.cs
@@ -0,0 +1,30 @@
+﻿namespace NeedForSpeed.Models.Cars
+{
+    using System.Text;
+
+    public class ShowCar : Car
+    {
+        private int stars;
+
+        public ShowCar(string brand, string model, int yearOfProduction, int horsePower, int acceleration, int suspension, int durability)
+            : base(brand, model, yearOfProduction, horsePower, acceleration, suspension, durability)
+        {
+        }
+
+        public override void Tune(int tuneIndex, string tuneAddon)
+        {
+            base.Tune(tuneIndex, tuneAddon);
+            this.stars += tuneIndex;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($"{this.stars} *");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Exams/Exam Preparation - 11.07.2017/NeedForSpeed/NeedForSpeed/Models/Garage.cs
@@ -0,0 +1,37 @@
+﻿namespace NeedForSpeed.Models
+{
+    using System.Collections.Generic;
+
+    public class Garage
+    {
+        private List<int> parkedCars;
+
+        public Garage()
+        {
+            this.ParkedCars = new List<int>();
+        }
+
+        public List<int> ParkedCars
+        {
+            get
+            {
+                return this.parkedCars;
+            }
+
+            private set
+            {
+                this.parkedCars = value;
+            }
+        }
+
+        public void ParkCar(int id)
+        {
+            this.ParkedCars.Add(id);
+        }
+
+        public void UnparkCar(int id)
+        {
+            this.ParkedCars.Remove(id);
+        }
+    }
+}
